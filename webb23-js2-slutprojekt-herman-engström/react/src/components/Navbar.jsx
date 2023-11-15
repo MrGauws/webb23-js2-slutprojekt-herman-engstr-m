43fd476c3ceef940659c179dbd9860e34c6940eb
MrGauws <120020363+MrGauws@users.noreply.github.com>
2023-11-15 14:05:20 +0100

    Update Navbar.jsx

--- webb23-js2-slutprojekt-herman-engström/react/src/components/Navbar.jsx
+++ webb23-js2-slutprojekt-herman-engström/react/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../public/logo/logo.png';
 
-const Navbar = ({ setShowCart, cartItems }) => {
+const Navbar = ({ setShowCart, cartItems, switchPage }) => {
   // Funktion för att räkna totalt antal varor i kundvagnen
   const calculateTotalItems = () => {
     return cartItems.reduce((total, item) => total + item.quantity, 0);
@@ -13,19 +13,17 @@ const Navbar = ({ setShowCart, cartItems }) => {
       <div className="logo-container">
         <img src={logo} alt="Logo" />
       </div>
-
       {/* Navigationslänkar */}
       <ul>
         <li>
           {/* Länk till produktsidan */}
-          <a href="#" onClick={() => setShowCart(false)}>
+          <a href="#" onClick={() => switchPage('products')}>
             Produkter
           </a>
         </li>
-
-        {/* Uppdaterad länk till kundvagnssidan med antal varor i kundvagnen */}
         <li>
-          <a href="#" onClick={() => setShowCart(true)}>
+          {/* Länk till kundvagnssidan*/}
+          <a href="#" onClick={() => switchPage('cart')}>
             Kundvagnen ({calculateTotalItems()})
           </a>
         </li>
